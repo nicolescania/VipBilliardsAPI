aba51320768e55a3f518be06d20f97365d02f9fe
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-09-02 19:18:37 -0400

    created branch model

---
+++ Server/Models/branch.ts
@@ -0,0 +1,30 @@
+// Step 1 - import mongoose - database adapter
+
+import mongoose, { Schema, Document } from 'mongoose';
+
+
+
+interface IBranches extends Document {
+    name: string;
+   
+  }
+  
+
+
+// Step 2 - Create a Schema that matches the data in the collection
+const brancheSchema: Schema<IBranches> = new Schema
+({
+
+        name: {
+        type: String,
+        require: true   }
+  
+})
+
+// Step 3- Create a Model using the Schema
+
+const Model = mongoose.model<IBranches>("Branches", brancheSchema);
+
+
+// Step 4 - Export the Model -> converts this file into a module
+export default Model;
